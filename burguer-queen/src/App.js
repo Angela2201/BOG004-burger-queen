814fef37f944357dfcbfb63f2889b8fad6efa857
angela <angelabecerraforero@gmail.com>
2022-06-13 20:08:44 -0500

    Se crea ruteo para Home y Wall y se instala firebase

--- burguer-queen/src/App.js
+++ burguer-queen/src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  Link,
+  Route,
+  Routes
+} from 'react-router-dom';
+import Home from './components/Home';
+import Wall from './components/Wall';
+
+//Se crean componente de "Página no encontrada"
+const NotFound = () => <h1>NotFound</h1>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/wall'>Wall</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/wall' element={<Wall />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
       </header>
     </div>
   );
