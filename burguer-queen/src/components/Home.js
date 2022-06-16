6cfd8cb55421100fb79016239a7c399dd510a8dc
angela <angelabecerraforero@gmail.com>
2022-06-15 20:25:55 -0500

    Se crea botón para cambiar de vista entre Home y Wall, además se crea un componente Home.module.css para los estilos

--- burguer-queen/src/components/Home.js
+++ burguer-queen/src/components/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
+import styles from './Home.module.css'
 
 const Home = () => {
+
+    let Navigate = useNavigate()
+    function changeView(e) {
+        e.preventDefault();
+        console.log("click");
+        Navigate("/Wall")
+    }
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div id="view-home" className={styles.viewHome}>
+            <form onSubmit={changeView}>
+                <button type="submit" className={styles.btnHome}>
+                    <img className={styles.imgHome} src="https://i.imgur.com/6h7OwHR.png"></img>
+                </button>
+            </form>
         </div>
     )
 }
